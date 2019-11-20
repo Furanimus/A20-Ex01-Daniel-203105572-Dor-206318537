1e6547970cc451c3aa11e6d6ce6587d9b47efb4a
furanimus <babitskyd@gmail.com>
2019-11-20 19:57:33 +0200

    Player movement and basic logic

--- A20 Ex01 Daniel 203105572 Dor 206318537/Game1.cs
+++ A20 Ex01 Daniel 203105572 Dor 206318537/Game1.cs
@@ -11,11 +11,37 @@ namespace A20_Ex01_Daniel_203105572_Dor_206318537
     {
         GraphicsDeviceManager m_Graphics;
         SpriteBatch m_SpriteBatch;
-        Texture2D m_PlayerShip;
+       
+        private Texture2D m_Background;
+        private const int k_BGWidth = 1024;
+        private const int k_BGHeight = 768;
+
+        //Player Ship parameters
+        private Texture2D m_PlayerShip;
+        private Vector2 m_PlayerShipPosition;
+        private int m_PlayerLives = 3;
+        private bool m_IsDead = false;
+        private const int k_PlayerVelocity = 110;
+        private const int k_PlayerShipWidth = 32;
+        private const int k_PlayerShipHeight = 32;
+
+        //Enemies parameters
+        private const int k_EnemyWidth = 32;
+        private const int k_EnemyHeight = 32;
+
+        //Mothership parameters
+        private const int k_MothershipWidth = 120;
+        private const int k_MothershipHeight = 32;
+        
+        //Bullets - class
+
 
         public Game1()
         {
             m_Graphics = new GraphicsDeviceManager(this);
+            m_Graphics.PreferredBackBufferWidth = k_BGWidth;  // set this value to the desired width of your window
+            m_Graphics.PreferredBackBufferHeight = k_BGHeight;   // set this value to the desired height of your window
+            m_Graphics.ApplyChanges();
             Content.RootDirectory = "Content";
         }
 
@@ -28,6 +54,7 @@ namespace A20_Ex01_Daniel_203105572_Dor_206318537
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
+            m_PlayerShipPosition = new Vector2(k_BGWidth - 64, k_BGHeight - 64); // Starting Position of player ship
 
             base.Initialize();
         }
@@ -42,6 +69,10 @@ namespace A20_Ex01_Daniel_203105572_Dor_206318537
             m_SpriteBatch = new SpriteBatch(GraphicsDevice);
 
             // TODO: use this.Content to load your game content here
+            m_Background = Content.Load<Texture2D>(@"Sprites\BG_Space01_1024x768");
+            m_PlayerShip = Content.Load<Texture2D>(@"Sprites\Ship01_32x32");
+
+
         }
 
         /// <summary>
@@ -58,12 +89,32 @@ namespace A20_Ex01_Daniel_203105572_Dor_206318537
         /// checking for collisions, gathering input, and playing audio.
         /// </summary>
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
+
         protected override void Update(GameTime gameTime)
         {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
             // TODO: Add your update logic here
+            // UpdateLeftmostEnemyBound();
+            // UpdateRightmostEnemyBound();
+            KeyboardState state = Keyboard.GetState();
+
+            if (state.IsKeyDown(Keys.Right))
+            {
+                if (m_PlayerShipPosition.X <= k_BGWidth - k_PlayerShipWidth)
+                {
+                    m_PlayerShipPosition.X += k_PlayerVelocity * (float)gameTime.ElapsedGameTime.TotalSeconds;
+                }
+            }
+
+            if (state.IsKeyDown(Keys.Left))
+            {
+                if (m_PlayerShipPosition.X >= 0)
+                {
+                    m_PlayerShipPosition.X -= k_PlayerVelocity * (float)gameTime.ElapsedGameTime.TotalSeconds;
+                }
+            }
 
             base.Update(gameTime);
         }
@@ -74,10 +125,19 @@ namespace A20_Ex01_Daniel_203105572_Dor_206318537
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.Clear(Color.White);
 
             // TODO: Add your drawing code here
+            m_SpriteBatch.Begin();
+            m_SpriteBatch.Draw(m_Background, new Vector2(0, 0), Color.White);
+            m_SpriteBatch.End();
 
+            m_SpriteBatch.Begin();
+            m_SpriteBatch.Draw(m_PlayerShip, m_PlayerShipPosition, Color.White);
+            m_SpriteBatch.End();
+
+
+            
             base.Draw(gameTime);
         }
     }
