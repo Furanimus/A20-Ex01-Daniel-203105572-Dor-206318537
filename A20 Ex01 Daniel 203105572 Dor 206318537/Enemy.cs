ad9d5ac36428dae227eb38f1e41fb4cb034622c7
xDtalx <dortal14@gmail.com>
2019-12-02 14:05:27 +0200

    Revert "Changes in enemies shots timing"
    This reverts commit e09fbf913ccc49589fea0572cfa0014c05a05aa1.

--- A20 Ex01 Daniel 203105572 Dor 206318537/Enemy.cs
+++ A20 Ex01 Daniel 203105572 Dor 206318537/Enemy.cs
@@ -5,12 +5,15 @@ namespace A20_Ex01_Daniel_203105572_Dor_206318537
 {
      public abstract class Enemy: Entity
      {
+          protected RandomBehavior m_RandomBehavior;
+
           public Enemy()
           {
                Velocity = 50;
                Height = 32;
                Width = 32;
                Lives = 1;
+               m_RandomBehavior = new RandomBehavior();
           }
 
           public int Score { get; set; }
@@ -27,6 +30,33 @@ namespace A20_Ex01_Daniel_203105572_Dor_206318537
                     {
                          Position += i_Direction * Velocity * (float)GameTime.ElapsedGameTime.TotalSeconds;
                     }
+               }
+          }
+
+          protected class RandomBehavior
+          {
+               protected Random m_Random;
+               protected readonly int r_RandomFactor = 10;
+               protected readonly int r_RandomMin = 0;
+               protected readonly int r_RandomMax = 5000;
+
+               public RandomBehavior()
+               {
+                    m_Random = new Random();
+               }
+
+               public RandomBehavior(int i_RandomFactor, int i_RandomMin, int i_RandomMax)
+               {
+                    m_Random = new Random();
+                    r_RandomFactor = i_RandomFactor;
+                    r_RandomMin = i_RandomMin;
+                    r_RandomMax = i_RandomMax;
+
+               }
+
+               public bool Roll()
+               {
+                    return m_Random.Next(r_RandomMin, r_RandomMax) < r_RandomFactor;
                }
           }
      }
