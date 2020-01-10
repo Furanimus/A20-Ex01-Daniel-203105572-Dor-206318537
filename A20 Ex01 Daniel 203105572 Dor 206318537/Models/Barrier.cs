7d3456dfb4bc4fb10b414e7f118463e3dca8675d
xdtalx <dortal14@gmail.com>
2020-01-10 17:45:58 +0200

    Fixed barriers with same texture

--- A20 Ex01 Daniel 203105572 Dor 206318537/Models/Barrier.cs
+++ A20 Ex01 Daniel 203105572 Dor 206318537/Models/Barrier.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Xna.Framework;
 using A20_Ex01_Daniel_203105572_Dor_206318537.Interfaces;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace A20_Ex01_Daniel_203105572_Dor_206318537.Models
 {
@@ -8,21 +9,16 @@ namespace A20_Ex01_Daniel_203105572_Dor_206318537.Models
           private const string k_AssetName = @"Sprites\Barrier_44x32";
           private const int k_DefaultWidth = 44;
           private const int k_DefaultHeight = 32;
+          private const int k_CallOrder = 6;
 
           public object GroupRepresentative { get; set; }
 
           public Barrier(Game i_Game)
-               : base(k_AssetName, i_Game)
+               : base(k_AssetName, i_Game, k_CallOrder)
           {
                this.Width = k_DefaultWidth;
                this.Height = k_DefaultHeight;
                this.Velocity = new Vector2(0, 0);
-          }
-
-          public override void Update(GameTime i_GameTime)
-          {
-
-               base.Update(i_GameTime);
           }
 
           public override void Collided(ICollidable i_Collidable)
